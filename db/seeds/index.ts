42d94f9b1a32e6b98d3d111132767c64ce299c25
Roni Ardiyanto <me@roniar.dev>
2025-02-03 07:49:46 +0700

    feat(db): update post schema and seeder

--- db/seeds/index.ts
+++ db/seeds/index.ts
@@ -1,14 +1,26 @@
 import { runPostsSeed } from "./post.seed";
 import { runUsersSeed } from "./user.seed";
 
-Promise.all([runPostsSeed(), runUsersSeed()])
-	.then(() => {
-		console.log("✅ All Seeds runned");
-	})
+const tasks = [runUsersSeed, runPostsSeed];
+
+tasks
+	.reduce(async (prevPromise, nextTask) => {
+		try {
+			// Tunggu sampai task sebelumnya selesai (success atau error)
+			await prevPromise;
+		} catch (err) {
+			// Tangkap error dari task sebelumnya, tapi lanjutkan eksekusi
+			console.error("Task sebelumnya error:", err);
+		}
+		// Jalankan task berikutnya dan return promise-nya
+		return nextTask();
+	}, Promise.resolve())
 	.catch((err) => {
-		console.error(err);
+		// Handle error terakhir (jika ada)
+		console.error("Task terakhir error:", err);
 	})
 	.finally(() => {
-		console.log("🚀 Shutting down...");
-		process.exit(0);
+		// Exit proses setelah semua task selesai
+		console.log("Semua task selesai dijalankan");
+		process.exit(0); // atau process.exit(1) jika ingin exit code error
 	});
