ba0750d002d8140c04363fcb7004cdc17a4243b8
Roni Ardiyanto <me@roniar.dev>
2025-02-11 16:55:51 +0700

    feat(server): cleaner route and handle onError

--- app/server.ts
+++ app/server.ts
@@ -3,10 +3,10 @@ import { cors } from "@elysiajs/cors";
 import { serverTiming } from "@elysiajs/server-timing";
 import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
-import { login } from "@/src/auth/usecase/login.usecase.ts";
 import { Logestic } from "logestic";
 import { rateLimit } from "elysia-rate-limit";
-import { logout } from "@/src/auth/usecase/logout.usecase.ts";
+import { posts } from "@/src/posts";
+import { auth } from "@/src/auth";
 
 export const app = new Elysia()
 	.use(Logestic.preset("fancy"))
@@ -20,7 +20,30 @@ export const app = new Elysia()
 	.use(bearer())
 	.use(cors())
 	.use(serverTiming())
-	.use(login)
-	.use(logout);
+	.use(auth)
+	.use(posts)
+	.onError(({ error, code, set }) => {
+		switch (code) {
+			case "VALIDATION": {
+				const resError = error.all as unknown as Array<
+					Record<string, string | number>
+				>;
+				const name = error.all[0] as unknown as Record<string, string>;
+
+				const errorType = name.type;
+
+				const err = resError.filter(
+					(err) =>
+						(Number(err?.type) || 0) >= 40 &&
+						(Number(err?.type) || 0) < 50 &&
+						err?.type,
+				);
+				set.status = 400;
+				return {
+					data: err,
+				};
+			}
+		}
+	});
 
 export type ElysiaApp = typeof app;
