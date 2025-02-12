0490d5c568f7b870f39496e98d901ee42fe98243
Roni Ardiyanto <me@roniar.dev>
2025-02-12 15:57:35 +0700

    feat(utils): update encrypt-response to support pagination

--- utils/encrypt-response/index.ts
+++ utils/encrypt-response/index.ts
@@ -3,7 +3,21 @@ import type { GeneralResponse } from "@/common/model/general-response";
 
 export const encryptResponse = (response: GeneralResponse) => {
 	if (!response.data) return;
-	const encryptedData = Crypto.encrypt(JSON.stringify(response.data));
+	let responseData: Record<string, string | number | undefined> = {
+		data: response.data,
+	};
+
+	if (response.total) {
+		responseData = {
+			...responseData,
+			total: response.total,
+			totalPage: response.totalPage,
+			page: response.page,
+			limit: response.limit,
+		};
+	}
+
+	const encryptedData = Crypto.encrypt(JSON.stringify(responseData));
 
 	return {
 		status: response.status,
