e0e447d34355a284df3897f32ed105657c70a5bb
Roni Ardiyanto <me@roniar.dev>
2025-02-12 15:56:39 +0700

    feat(utils): pagination on general-response

--- common/model/general-response.ts
+++ common/model/general-response.ts
@@ -4,6 +4,10 @@ export const generalResponse = t.Object({
 	status: t.Boolean(),
 	message: t.String(),
 	data: t.Any(),
+	total: t.Optional(t.Number()),
+	totalPage: t.Optional(t.Number()),
+	page: t.Optional(t.Number()),
+	limit: t.Optional(t.Number()),
 });
 
 export type GeneralResponse = typeof generalResponse.static;
