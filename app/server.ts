46e0c40c57221e5fd7ffce93772429ab9d1e9be5
Roni Ardiyanto <me@roniar.dev>
2025-03-09 12:42:43 +0700

    feat(logging): winston logging daily rotate

--- app/server.ts
+++ app/server.ts
@@ -3,6 +3,12 @@ import { Logestic } from "logestic";
 import { cors } from "@elysiajs/cors";
 import { serverTiming } from "@elysiajs/server-timing";
 import { swagger } from "@elysiajs/swagger";
+import {
+	ElysiaLogging,
+	type Logger,
+	LogFormat,
+} from "@otherguy/elysia-logging";
+import { createLogger, transports, format } from "winston";
 
 import { config } from "./config";
 
@@ -10,6 +16,7 @@ import { posts } from "@/src/posts";
 import { auth } from "@/src/auth";
 import { users } from "@/src/users";
 import { permissions } from "@/src/permissions";
+import logger from "@/utils/logger";
 
 export const app = new Elysia({
 	serve: {
@@ -26,29 +33,39 @@ export const app = new Elysia({
 		}),
 	)
 	.use(serverTiming())
-	.use(auth)
-	.use(posts)
-	.use(users)
-	.use(permissions)
 	.onError(({ error, code, set }) => {
 		switch (code) {
 			case "VALIDATION": {
-				const resError = error.all as unknown as Array<
-					Record<string, string | number>
-				>;
-
+				const resError = error.all as unknown as Array<Record<string, string | number>>;
+				const name = error.all[0] as unknown as Record<string, string>;
 				const err = resError.filter(
 					(err) =>
 						(Number(err?.type) || 0) >= 40 &&
 						(Number(err?.type) || 0) < 50 &&
 						err?.type,
 				);
+
 				set.status = 400;
 				return {
-					data: err,
+					status: false,
+					message: name.summary,
+					err: err,
+				};
+			}
+
+			case "INTERNAL_SERVER_ERROR": {
+				logger.error(error);
+				return {
+					status: false,
+					message: error.message,
+					err: error,
 				};
 			}
 		}
-	});
+	})
+	.use(auth)
+	.use(posts)
+	.use(users)
+	.use(permissions);
 
 export type ElysiaApp = typeof app;
